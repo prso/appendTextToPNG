87479dda0f7dbea3bbf15981d7a23b6f21ee2b43
prso <prso@users.noreply.github.com>
2016-05-28 00:24:21 +0200

    Add a mode with inline text data and output to stdout

--- appendTextToPNG.py
+++ appendTextToPNG.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 """
 
 Length 	Chunk type 	Chunk data 	CRC
@@ -13,7 +14,7 @@ import struct
 import zlib
 import posixpath
 
-from sys import argv, stdin
+from sys import argv,stdout
 
 if len(argv) == 3:
     if posixpath.exists(argv[1]) and posixpath.exists(argv[2]):
@@ -23,16 +24,31 @@ if len(argv) == 3:
 	print "files do not exist"
 	raise SystemExit
 
-    
+elif len(argv) == 4:
+    if posixpath.exists(argv[3]):
+        iname = None
+        key = argv[1]
+        text = argv[2]
+        pname = argv[3]
+    else:
+        print "file does not exist"
+        raise SystemExit
+
 else: # usage
     print "Usage: %s <file> <png>" % argv[0]
-    print "\tReads data from <file> and appends its data as text-chunk as second-to-last chunk to the given PNG file"
-    print "\t(last chunk is always the magic PNG ending)"
+    print "\tReads data from <file> and appends its data as text-chunk (tEXt) to the <png> file\n"
+    print "Usage: %s <key> <text> <png>" % argv[0]
+    print "\tUse inline <key> and <text> to append them as text-chunk (tEXt) to the <png> file\n"
+    print "\tthe text is added as second-to-last chunk (last chunk is always the magic PNG ending)"
+    print "\tthe new PNG data is written to standard output"
     raise SystemExit
 
-ihandle = open(iname, "rb")
-input_data = ihandle.read()
-ihandle.close()
+if iname is None:
+    input_data = (key + "\x00" + text)
+else:
+    ihandle = open(iname, "rb")
+    input_data = ihandle.read()
+    ihandle.close()
 
 
 f = open(pname, 'rb')
@@ -63,9 +79,4 @@ chunk_crc = struct.pack("!I", chunk_crc)
 newchunk = (length + chunktype + input_data + chunk_crc)
 
 # new image = old image + new chunk before "end of image" chunk
-new_image = img_data_pre + newchunk + img_data_post
-
-newfile = open(iname+'.png','wb')
-newfile.write(new_image)
-newfile.close()
-print "written to", iname+'.png'
+stdout.write(img_data_pre + newchunk + img_data_post)
